3100270612dadbb4b8ef0b1ed7825250f559f3e1
Hubert Wajda <hubert.wajda03@gmail.com>
2024-03-12 22:30:27 +0100

    multithreads

--- Lab3_GniazdaSieciowe/src/main/java/org/example/ClientHandler.java
+++ Lab3_GniazdaSieciowe/src/main/java/org/example/ClientHandler.java
@@ -1,8 +1,46 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class ClientHandler implements Runnable {
+
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public ClientHandler(Socket socket, ObjectOutputStream out, ObjectInputStream in) {
+        this.socket = socket;
+        this.out = out;
+        this.in = in;
+    }
+
     @Override
     public void run() {
-
+        try {
+            out.writeObject("Ready");
+            out.writeObject("Ready for messages");
+            var number = in.readObject();
+            for (int i = 0; i <(int) number; i++) {
+                Message msg = (Message) in.readObject();
+                System.out.println(msg);
+            }
+            out.writeObject("Finished");
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private void close() throws IOException {
+        in.close();
+        out.close();
+    }
+
 }
--- Lab3_GniazdaSieciowe/src/main/java/org/example/Server.java
+++ Lab3_GniazdaSieciowe/src/main/java/org/example/Server.java
@@ -5,46 +5,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private static final String STARTSERVER = "Ready";
-    private static final String READY = "Ready for message";
-    private static final String END = "Finished";
-
     public static final int PORT = 1234;
     private ServerSocket serverSocket;
-    private ObjectOutputStream out;
-    private ObjectInputStream in;
 
     public Server() throws IOException {
         serverSocket = new ServerSocket(PORT);
     }
-    private void initConnections() throws IOException {
-        Socket clientSocket = serverSocket.accept();
-        out = new ObjectOutputStream(clientSocket.getOutputStream());
-        in = new ObjectInputStream(clientSocket.getInputStream());
-    }
 
-    private void close() throws IOException {
-        in.close();
-        out.close();
-    }
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
         Server server = new Server();
         System.out.println("Server Started.");
-        try {
-            while (true) {
-                server.initConnections();
-                server.out.writeObject("Ready");
-                server.out.writeObject("Ready for messages");
-                var number = server.in.readObject();
-                for (int i = 0; i <(int) number; i++) {
-                    Message msg = (Message) server.in.readObject();
-                    System.out.println(msg);
-                }
-                server.close();
-            }
-        } finally {
-            server.close();
+        while (true) {
+            Socket clientSocket = server.serverSocket.accept();
+            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+
+            ClientHandler clientHandler = new ClientHandler(clientSocket, out, in);
+            Thread thread = new Thread(clientHandler);
+            thread.start();
         }
+
     }
 }
