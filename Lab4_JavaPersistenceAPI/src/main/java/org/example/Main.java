57d4a65a94f8f0f70a29a37c60b423b2f96b791b
Hubert Wajda <hubert.wajda03@gmail.com>
2024-03-18 15:54:44 +0100

    config and simple insertion

--- Lab4_JavaPersistenceAPI/src/main/java/org/example/Main.java
+++ Lab4_JavaPersistenceAPI/src/main/java/org/example/Main.java
@@ -2,13 +2,26 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf =
-                Persistence.createEntityManagerFactory("namePU");
+                Persistence.createEntityManagerFactory("test");
         EntityManager em = emf.createEntityManager();
 
+        EntityTransaction entityTransaction = em.getTransaction();
+        entityTransaction.begin();
+        em.persist(new Mage("Hubert", 10, null));
+        entityTransaction.commit();
+
+
+        List<Mage> mages=  em.createNativeQuery("SELECT  * FROM Mage", Mage.class).getResultList();
+
+        em.close();
+        emf.close();
     }
 }
