f8820319eec029a02f132b6c5b5ed99162f523a9
Hubert Wajda <hubert.wajda03@gmail.com>
2024-03-20 08:41:19 +0100

    finish lab

--- Lab4_JavaPersistenceAPI/src/main/java/org/example/Main.java
+++ Lab4_JavaPersistenceAPI/src/main/java/org/example/Main.java
@@ -40,7 +40,43 @@ public class Main {
             instruction = scanner.nextLine();
             entityTransaction.begin();
             if(instruction.equals("add")){
-
+                System.out.println("Mage or Tower");
+                instruction = scanner.nextLine();
+                if(instruction.equals("mage")){
+                    String name = "";
+                    String towerName = "";
+                    int level = 0;
+                    System.out.println("Name:");
+                    name = scanner.nextLine();
+                    System.out.println("Level:");
+                    level = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Tower name:");
+                    towerName = scanner.nextLine();
+                    if(towerName.length()>0){
+                        Tower tower1 = em.createQuery("Select t From Tower t WHERE t.name= :name", Tower.class)
+                                .setParameter("name", towerName)
+                                .getSingleResult();
+                        Mage mage1 = new Mage(name, level, tower1);
+                        tower1.addMage(mage1);
+                        em.persist(mage1);
+                    }
+                    else{
+                        Mage mage1 = new Mage(name, level, null);
+                        em.persist(mage1);
+                    }
+                }
+                else if(instruction.equals("tower")){
+                    String name = "";
+                    int height = 0;
+                    System.out.println("Name:");
+                    name = scanner.nextLine();
+                    System.out.println("Height:");
+                    height = scanner.nextInt();
+                    scanner.nextLine();
+                    Tower tower1 = new Tower(name, height, new ArrayList<>());
+                    em.persist(tower1);
+                }
             }
             else if(instruction.equals("update")){
 
@@ -49,7 +85,7 @@ public class Main {
                 System.out.println("Mage or Tower");
                 instruction = scanner.nextLine();
                 if(instruction.equals("mage")){
-                    System.out.println("Namge: ");
+                    System.out.println("Name: ");
                     instruction = scanner.nextLine();
                     Mage mage1 = em.createQuery("Select m From Mage m WHERE m.Name= :name", Mage.class)
                             .setParameter("name",instruction)
@@ -62,6 +98,13 @@ public class Main {
                 else if(instruction.equals("tower")){
                     System.out.println("Name: ");
                     instruction = scanner.nextLine();
+                    Tower tower1 = em.createQuery("Select t From Tower t WHERE t.name= :name", Tower.class)
+                            .setParameter("name", instruction)
+                            .getSingleResult();
+                    for(Mage mage1: tower1.getMages()){
+                        mage1.setTower(null);
+                    }
+                    em.remove(tower1);
                 }
 
             }
@@ -70,6 +113,23 @@ public class Main {
                 System.out.println(mages);
                 List<Tower> towers = em.createQuery("select t FROM Tower t", Tower.class).getResultList();
                 System.out.println(towers);
+            }
+            else if(instruction.equals("display tower")){
+                String name = "";
+                int minLevel = 0;
+                System.out.println("Name:");
+                name = scanner.nextLine();
+                System.out.println("Min Level:");
+                minLevel = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Mages");
+                List<Mage> mages = em.createQuery("SELECT m FROM Mage m WHERE m.tower.name =: name and m.level >: minLevel")
+                        .setParameter("name", name)
+                        .setParameter("minLevel", minLevel)
+                        .getResultList();
+                for(Mage mage1: mages){
+                    System.out.println(mage1);
+                }
             }
             else System.out.println("Wrong instruction!");
             entityTransaction.commit();
