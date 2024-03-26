2cb25afae001eb4c23d9a5cb2986f323b660927b
Hubert Wajda <hubert.wajda03@gmail.com>
2024-03-26 17:32:20 +0100

    controller test

--- Lab5_TestyJednostkowe/src/test/java/mage/MageControllerTest.java
+++ Lab5_TestyJednostkowe/src/test/java/mage/MageControllerTest.java
@@ -1,4 +1,45 @@
 package mage;
 
+import org.example.mage.Mage;
+import org.example.mage.MageController;
+import org.example.mage.MageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class MageControllerTest {
+    MageRepository mageRepository;
+    MageController mageController;
+
+    @BeforeEach
+    void setUp(){
+        mageRepository = mock(MageRepository.class);
+        mageController = new MageController(mageRepository);
+    }
+
+    @Test
+    public void findTest(){
+        //Arrange
+        Mage mage = new Mage("test", 1);
+        //Act
+        when(mageRepository.find("test")).thenReturn(Optional.of(mage));
+        String result = mageController.find("test");
+        //Assert
+        assertEquals(mage.toString(), result);
+    }
+
+    @Test
+    public void notFoundTest(){
+        //Arrange
+        //Act
+        when(mageRepository.find("test")).thenReturn(Optional.empty());
+        String result = mageController.find("test");
+        //Assert
+        assertEquals("not found", result);
+    }
 }
